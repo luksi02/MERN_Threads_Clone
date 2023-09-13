6f6d64bb80cb7ebf36870f032a7c932fd02f8a5b
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-09-13 22:26:57 +0200

    13

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,25 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import {fetchUser} from "@/lib/actions/user.actions";
+import PostThread from "@/components/forms/PostThread";
+
+
+async function Page() {
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    return (
+        <>
+            <h1 className="head-text">Create Thread</h1>
+
+            <PostThread userId={userInfo._id} />
+        </>
+    )
+}
+
+export default Page;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,41 @@
-import Image from 'next/image'
+// "use client"
 
-export default function Home() {
-  return (
-      <main>
-        <h1>Luksi02's Threads clone</h1>
-      </main>
-  )
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+
+export default async function Home() {
+
+    const results = await fetchPosts();
+    const user = await currentUser();
+
+    // console.log(results);
+
+    return (
+        <>
+            <h1 className="head-text text-left">Home</h1>
+
+            <section className="mt-9 flex flex-col gap-10">
+                {results.posts.length === 0 ? (
+                    <p className="no-result">No threads found</p>
+                ) : (
+                    <>
+                        {results.posts.map((post) => (
+                            <ThreadCard
+                                key={post._id}
+                                id={post._id}
+                                currentUserId={user?.id}
+                                parentId={post.parentId}
+                                content={post.text}
+                                author={post.author}
+                                community={post.community}
+                                createdAt={post.createdAt}
+                                comments={post.children}
+                            />
+                        ))}
+
+                    </>
+                )}
+                </section>
+        </>
+    )
 }
