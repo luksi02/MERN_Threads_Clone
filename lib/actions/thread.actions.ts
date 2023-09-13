e1f0d7abf835d305844ecdf54b960a2a810950a5
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-09-14 00:57:44 +0200

    14

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,9 @@
+"use server"
+
 import {connectToDB} from "@/lib/mongoose";
+import Thread from "@/lib/models/thread.model";
+import User from "@/lib/models/user.model";
+import {revalidatePath} from "next/cache";
 
 interface Params {
     text: string,
@@ -10,8 +15,92 @@ interface Params {
 
 
 export async function createThread ({ text, author, communityId, path}: Params){
+    try {
+        connectToDB();
 
+        const createdThread = await Thread.create({
+            text,
+            author,
+            community: null,
+        });
+
+//     Update user model
+
+        await User.findByIdAndUpdate(author, {
+            $push: { threads: createdThread._id }
+        })
+
+        revalidatePath(path);
+    } catch (error: any) {
+        throw new Error(`Error creating new thread: ${error.message}`)
+    }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize =20) {
     connectToDB();
 
-    const createdThread = await Thread.create();
+    // calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Fetch the posts that have no parents (top-level threads)
+    const postQuery = Thread.find({ parentId: { $in: [null, undefined ] } } )
+        .sort({ createdAt: 'desc'})
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: '_id name parentId image'
+            }
+        })
+
+    const totalPostCount = await Thread.countDocuments({ parentId: { $in: [null, undefined ] } } )
+
+    const posts = await postQuery.exec();
+
+    const isNext = totalPostCount > skipAmount + posts.length;
+
+    return { posts, isNext }
+}
+
+export async function fetchThreadById( id : string ) {
+    connectToDB();
+
+    // TODO: populate community! Population too low to maintain civilisation!
+    try {
+        const thread = await Thread.findById(id)
+            .populate({
+                path: 'author',
+                model: User,
+                select: "_id id name image"
+            })
+            // HERE COMES THE POPULATION INCEPTION!
+            .populate({
+              path: 'children',
+              populate: [
+                  {
+                      path: 'author',
+                      model: User,
+                      select: "_id id name parentId image"
+                  },
+                  {
+                      path: 'children',
+                      model: Thread,
+                      populate: {
+                          path: 'author',
+                          model: User,
+                          select: "_id id name parentId image"
+                      }
+                  }
+              ]
+            })
+
+    } catch (error) {
+
+        console.log(error)
+
+    }
 }
